1b656793ae467437183a854859c9bac43236d64e
RUPESH KUMAR <rupeshyadav9661628796@gmail.com>
2021-04-24 19:36:08 +0530

    23-4-2021

---
+++ multiplication of two integers by user value.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main ()
+{
+	int num1 , num2 , result ; 
+	printf( " entet the num1 is " , num1);
+	scanf ( "%d" , &num1);
+	printf( " enter the num2 is " , num2);
+	scanf( "%d", &num2);
+	result = num1 * num2 ;
+	printf ( " the multiplication of %d and %d is %d ", num1 , num2 , result);
+	
+}
